aa5354d8b0929430b4f245e37bc0622f17de5c0c
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-06-24 19:29:02 +0530

    Create install.sh

---
+++ bin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/kpu/kenlm.git
+cd kenlm
+mkdir -p build && cd build
+cmake .. 
+make -j 16
+cd ..
+export KENLM_ROOT=$PWD
+# export USE_CUDA=0 ## for cpu
+cd ..
